83fc2ece56a11dc0f0cf2f83baa1c0324ce5e87b
Jim <jimstacey.spb@gmail.com>
2022-02-21 12:11:33 +0300

    i18n troubles

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('body')
 <div class="p-8">
+    {{__('title')}}
+    {{__('common.title')}}
     <h1 class="text-3xl font-bold">Hello world!</h1>
 </div>
 @endsection
---
+++ source/lang/en/common.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'title' => "Very long page title"
+];
